6c440dda4efb1de7735e370bff4e9a4e525345b7
Jay Sprout <jotasprout@gmail.com>
2022-10-19 01:31:43 -0400

    troubleshooting via Q&A

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,7 +25,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite()
+            })
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options){}
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
