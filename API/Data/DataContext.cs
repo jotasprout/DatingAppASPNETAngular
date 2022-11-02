f7532fc0f7f24d6f7602321a04c939a06216e40d
Jay Sprout <jotasprout@gmail.com>
2022-11-02 09:20:29 -0400

    created Data and Entities classes

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace Data
+namespace API.Data
 {
     public class DataContext : DbContext
     {
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,15 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+
 namespace API
 {
     public class Startup
@@ -27,8 +30,8 @@ namespace API
         {
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlite()
-            })
+                options.UseSqlite();
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
