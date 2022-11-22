fb04f398b776f21dc4cf56ba2331782d297a3fa6
Jay Sprout <jotasprout@gmail.com>
2022-11-22 14:48:16 -0500

    Hello CS and such

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        public UsersController(DataContext context)
+        {
+        }
+    }
+}
